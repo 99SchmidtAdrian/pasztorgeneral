df2fbc6c3d1aae8fe55eb5848dce3bc96a92b616
99SchmidtAdrian <84144084+99SchmidtAdrian@users.noreply.github.com>
2021-12-02 08:56:50 +0100

    getting rid of getElementById

--- static/js/szolgaltatasok.js
+++ static/js/szolgaltatasok.js
@@ -1,6 +1,6 @@
 const changeHeight = (id) => {
-        const element = document.getElementById('type__p-' + id);
-        const button = document.getElementById('type__button-' + id);
+        const element = document.querySelector('#type__p-' + id);
+        const button = document.querySelector('#type__button-' + id);
         if (element.classList.contains('type__p-visible')) {
                 element.classList.remove('type__p-visible')
                 button.classList.remove('type__button-rotated')
