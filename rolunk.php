7ee038f65916d560d9e21cf36b3207be97b9ff97
99SchmidtAdrian <84144084+99SchmidtAdrian@users.noreply.github.com>
2021-12-02 08:51:26 +0100

    about us page

---
+++ rolunk.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>PÁSZTOR GENERAL TAKI - A takarító cég</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Gothic&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Prosto+One&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="./static/css/rolunk.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="icon" href="./static/images/title_logo.png" type="image/icon type" />
+</head>
+
+<body>
+  <div id="main-container">
+    <?php require_once './components/header.php' ?>
+    <!-- content div start -->
+    <div class="hero__main" id="hero__main">
+      <div class="hero__content">
+        <h1 class="hero__title">Ha takartó cég, akkor Pásztor General Taki!</h1>
+        <a href="./rolunk.php" class="hero__button">Vegye fel velünk a kapcsolatot!</a>
+      </div>
+    </div>
+    <div class="main__wrapper" id="main__wrapper">
+      <div class="main__first-content">
+        <div class="logo">
+          <img class="logo-img" src="./static/images/logo.png" alt="Pásztor General Taki - logo">
+        </div>
+        <div class="content">
+          <h2 class="title">Lorem Ipsum!</h2>
+        <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, possimus doloremque,
+          quod sed iure saepe officiis, quo neque cumque deleniti cum! Velit quo nemo at error fuga modi! Voluptas,
+          necessitatibus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni reiciendis ipsum rerum nemo
+          quaerat autem, nihil tenetur harum sequi molestiae culpa minima sit fugit, quis at fugiat adipisci sapiente.
+          Nulla?</p>
+          </div>
+      </div>
+      <div class="main__content">
+        <h2 class="title">Lorem Ipsum</h2>
+        <p class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In dolor at modi non ducimus similique dolorem
+          alias voluptatum dolorum pariatur, consequuntur sapiente cupiditate tempore iste blanditiis vero omnis
+          corporis dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, labore perferendis ad
+          minima fuga quos illo distinctio error sed reprehenderit explicabo cumque perspiciatis ut totam aspernatur
+          ratione unde, amet laudantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odit natus
+          velit, expedita nesciunt saepe? Iste quos sit, reiciendis nulla ab voluptate illum quo quaerat voluptates
+          eaque corrupti fuga odio?</p>
+      </div>
+      <div class="main__content">
+        <h2 class="title">Lorem Ipsum</h2>
+        <p class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In dolor at modi non ducimus similique dolorem
+          alias voluptatum dolorum pariatur, consequuntur sapiente cupiditate tempore iste blanditiis vero omnis
+          corporis dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, labore perferendis ad
+          minima fuga quos illo distinctio error sed reprehenderit explicabo cumque perspiciatis ut totam aspernatur
+          ratione unde, amet laudantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odit natus
+          velit, expedita nesciunt saepe? Iste quos sit, reiciendis nulla ab voluptate illum quo quaerat voluptates
+          eaque corrupti fuga odio?</p>
+      </div>
+      <div class="main__content">
+        <h2 class="title">Lorem Ipsum</h2>
+        <p class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In dolor at modi non ducimus similique dolorem
+          alias voluptatum dolorum pariatur, consequuntur sapiente cupiditate tempore iste blanditiis vero omnis
+          corporis dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, labore perferendis ad
+          minima fuga quos illo distinctio error sed reprehenderit explicabo cumque perspiciatis ut totam aspernatur
+          ratione unde, amet laudantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odit natus
+          velit, expedita nesciunt saepe? Iste quos sit, reiciendis nulla ab voluptate illum quo quaerat voluptates
+          eaque corrupti fuga odio?</p>
+      </div>
+      <div class="main__content">
+        <h2 class="title">Lorem Ipsum</h2>
+        <p class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In dolor at modi non ducimus similique dolorem
+          alias voluptatum dolorum pariatur, consequuntur sapiente cupiditate tempore iste blanditiis vero omnis
+          corporis dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, labore perferendis ad
+          minima fuga quos illo distinctio error sed reprehenderit explicabo cumque perspiciatis ut totam aspernatur
+          ratione unde, amet laudantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odit natus
+          velit, expedita nesciunt saepe? Iste quos sit, reiciendis nulla ab voluptate illum quo quaerat voluptates
+          eaque corrupti fuga odio?</p>
+      </div>
+    </div>
+    <!-- content div end -->
+    <?php require_once './components/footer.php' ?>
+  </div>
+  <script src="static/js/main.js"></script>
+  <script src="static/js/rolunk.js"></script>
+</body>
+
+</html>
