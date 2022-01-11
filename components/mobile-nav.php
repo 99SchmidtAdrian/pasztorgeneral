477a613529d9d8e11db987b95e878b57317edc9b
99SchmidtAdrian <84144084+99SchmidtAdrian@users.noreply.github.com>
2022-01-11 13:04:05 +0100

    removing unuused div

--- components/mobile-nav.php
+++ components/mobile-nav.php
@@ -7,18 +7,13 @@
   </div>
   <nav id="mobile-menu__dropdown">
     <ul>
-    <li><a href="./" id="fooldal-mobile-button">Főoldal</a></li>
-    <li><a href="./rolunk.php" id="rolunk-mobile-button">Rólunk</a></li>
-    <li><a href="./galeria.php" id="galeria-mobile-button">Galéria</a></li>
-    <li><a href="./szolgaltatasok.php" id="szolgaltatasok-mobile-button">Szolgáltatások</a></li>
-    <li><a href="./kapcsolat.php" id="kapcsolat-mobile-button">Kapcsolat</a></li>
-    <li><a href="./karrier.php" id="karrier-mobile-button">Karrier</a></li>
+      <li><a href="./" id="fooldal-mobile-button">Főoldal</a></li>
+      <li><a href="./rolunk.php" id="rolunk-mobile-button">Rólunk</a></li>
+      <li><a href="./galeria.php" id="galeria-mobile-button">Galéria</a></li>
+      <li><a href="./szolgaltatasok.php" id="szolgaltatasok-mobile-button">Szolgáltatások</a></li>
+      <li><a href="./kapcsolat.php" id="kapcsolat-mobile-button">Kapcsolat</a></li>
+      <li><a href="./karrier.php" id="karrier-mobile-button">Karrier</a></li>
     </ul>
   </nav>
 </div>
 <!-- mobile menu end-->
-<!-- back to top start -->
-<div>
-  
-</div>
-<!-- back to top end -->
