69c77ed6b4990d0288224cf7fa8c4034a1cef56b
99SchmidtAdrian <84144084+99SchmidtAdrian@users.noreply.github.com>
2022-01-11 13:04:15 +0100

    google analztics

--- components/footer.php
+++ components/footer.php
@@ -6,8 +6,21 @@
         </a>
         <ul class="footer__contacts">
             <li><a class="footer__info" href="tel: +36300815903">Telefonszám: +36-30/081-5903</a></li>
-            <li><a class="footer__info" href="mailto: pasztorgeneraltaki@gmail.com">Email: pasztorgeneraltaki@gmail.com</a></li>
+            <li><a class="footer__info" href="mailto: pasztorgeneraltaki@gmail.com">Email:
+                    pasztorgeneraltaki@gmail.com</a></li>
             <li><span class="footer__info">Budapest 1085 József krt. 69.Fsz1</span></li>
         </ul>
     </footer>
     <!-- footer end -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-81L0777CFB"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-81L0777CFB');
+    </script>
